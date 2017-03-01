6ce5fc582e69b5ee5770dba707673bafc84d1cbc
Achilles Dinglasan <adinglasan@reekoh.com>
2017-03-01 13:55:51 +0800

    migrated to v3

--- app.js
+++ app.js
@@ -1,135 +1,86 @@
-'use strict';
+'use strict'
 
-var uuid          = require('node-uuid'),
-	async         = require('async'),
-	isArray       = require('lodash.isarray'),
-	request       = require('request'),
-	isEmpty       = require('lodash.isempty'),
-	platform      = require('./platform'),
-	isPlainObject = require('lodash.isplainobject'),
-	get = require('lodash.get'),
-	config;
+let reekoh = require('reekoh')
+let _plugin = new reekoh.plugins.Connector()
+let async = require('async')
+let get = require('lodash.get')
+let isArray = require('lodash.isarray')
+let isEmpty = require('lodash.isempty')
+let isPlainObject = require('lodash.isplainobject')
+let uuid = require('node-uuid')
+let request = require('request')
 
 let sendData = (data, callback) => {
-	let sysid = '';
+  let sysid = ''
 
-	if (!isEmpty(config.id_field)) {
-		sysid = data[config.id_field];
-		delete data[config.id_field];
-	}
+  if (!isEmpty(_plugin.config.idField)) {
+    sysid = data[_plugin.config.idField]
+    delete data[_plugin.config.idField]
+  }
 
-	if (isEmpty(sysid))
-		sysid = uuid.v4();
+  if (isEmpty(sysid)) { sysid = uuid.v4() }
 
-	data.sys_id = sysid;
+  data['sys_id'] = sysid
 
-	request.post({
-		url: `${config.url}/api/now/table/${config.data_table_name}`,
-		auth: {
-			user: config.username,
-			pass: config.password
-		},
-		json: data
-	}, (error, response, body) => {
-		if (isEmpty(get(body, 'error'))) {
-			platform.log(JSON.stringify({
-				title: 'Data inserted to ServiceNow table.',
-				data: data
-			}));
-			callback();
-		}
-        else
-		    callback(body.error);
-	});
-};
+  request.post({
+    url: `${_plugin.config.url}/api/now/table/${_plugin.config.dataTableName}`,
+    auth: {
+      user: _plugin.config.username,
+      pass: _plugin.config.password
+    },
+    json: data
+  }, (error, response, body) => {
+    if (error) return callback(error)
 
-let addDevice = (device, callback) => {
-	request.post({
-		url: `${config.url}/api/now/table/${config.device_table_name}`,
-		auth: {
-			user: config.username,
-			pass: config.password
-		},
-		json: device
-	}, (error, response, body) => {
-		if (isEmpty(body.error)) {
-			platform.log(JSON.stringify({
-				title: 'Device added to ServiceNow table.',
-				data: device
-			}));
-		}
+    if (isEmpty(get(body, 'error'))) {
+      _plugin.log(JSON.stringify({
+        title: 'Data inserted to ServiceNow table.',
+        data: data
+      }))
+      callback()
+    } else {
+      callback(body.error)
+    }
+  })
+}
 
-		callback(body.error);
-	});
-};
+/**
+ * Emitted when device data is received.
+ * This is the event to listen to in order to get real-time data feed from the connected devices.
+ * @param {object} data The data coming from the device represented as JSON Object.
+ */
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (!isEmpty(error)) {
+        console.error(error)
+        _plugin.logException(new Error(error))
+      }
+    })
+  } else if (isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (!isEmpty(error)) {
+        console.error(error)
+        _plugin.logException(new Error(error))
+      }
+    })
+  } else {
+    _plugin.logException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`))
+  }
+})
 
-platform.on('data', function (data) {
-	if (isPlainObject(data)) {
-		sendData(data, (error) => {
-			if (!isEmpty(error)) {
-				console.error(error);
-				platform.handleException(new Error(error));
-			}
-		});
-	}
-	else if (isArray(data)) {
-		async.each(data, (datum, done) => {
-			sendData(datum, done);
-		}, (error) => {
-			if (!isEmpty(error)) {
-				console.error(error);
-				platform.handleException(new Error(error));
-			}
-		});
-	}
-	else
-		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
-});
+/**
+ * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
+ */
+_plugin.once('ready', () => {
+  if (_plugin.config.url.endsWith('/')) {
+    _plugin.config.url = _plugin.config.url.slice(0, -1)
+  }
 
-platform.on('adddevice', function (device) {
-	addDevice({
-		sys_id: device._id,
-		u_name: device.name
-	}, (error) => {
-		if (!isEmpty(error)) {
-			console.error(error);
-			platform.handleException(new Error(error));
-		}
-	});
-});
+  _plugin.log('ServiceNow Connector has been initialized.')
+  _plugin.emit('init')
+})
 
-platform.on('removedevice', function (device) {
-	request.del({
-		url: `${config.url}/api/now/table/${config.device_table_name}/${device._id}`,
-		auth: {
-			user: config.username,
-			pass: config.password
-		},
-		json: true
-	}, (error, response, body) => {
-		if (isEmpty(body.error)) {
-			platform.log(JSON.stringify({
-				title: 'Device removed from ServiceNow table.',
-				data: device
-			}));
-		}
-		else {
-			console.error(error);
-			platform.handleException(new Error(error));
-		}
-	});
-});
-
-platform.once('close', function () {
-	platform.notifyClose();
-});
-
-platform.once('ready', function (options) {
-	config = options;
-
-	if (config.url.endsWith('/'))
-		config.url = config.url.slice(0, -1);
-
-	platform.notifyReady();
-	platform.log('ServiceNow Connector has been initialized.');
-});
+module.exports = _plugin
